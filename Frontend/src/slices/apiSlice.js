edc876994700c482d6435a3c1674d456cbf589b5
Muhammed Bilal <130375693+mhmmd-Bilal@users.noreply.github.com>
2025-12-06 10:52:05 +0530

    Set base URL for API slice

--- Frontend/src/slices/apiSlice.js
+++ Frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://edure-recording-backend.onrender.com" }),
   tagTypes: [],
   endpoints: () => ({}),
 });
